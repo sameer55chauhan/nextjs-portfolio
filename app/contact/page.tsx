d9082c6b3b68de9e4d71b726fd3ed44ddb9c2d32
Sameer Chauhan <sameer55chauhan@gmail.com>
2024-03-17 22:51:04 +0530

    feat: added projects and metadata

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,24 +5,24 @@ import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
-	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
-	},
-	{
-		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
-		label: "Email",
-		handle: "dev@chronark.com",
-	},
-	{
-		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
-		label: "Github",
-		handle: "chronark",
-	},
+  {
+    icon: <Twitter size={20} />,
+    href: "https://twitter.com/SameerC97290733",
+    label: "Twitter",
+    handle: "@SameerC97290733",
+  },
+  {
+    icon: <Mail size={20} />,
+    href: "mailto:sameer55chauhan@gmail.com",
+    label: "Email",
+    handle: "sameer55chauhan@gmail.com",
+  },
+  {
+    icon: <Github size={20} />,
+    href: "https://github.com/sameer55chauhan",
+    label: "Github",
+    handle: "sameer55chauhan",
+  },
 ];
 
 export default function Example() {
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,18 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "Sameer Chauhan",
+    template: "%s | self.chauhan.org.in",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "I am a Full Stack Developer from India.",
   openGraph: {
-    title: "chronark.com",
-    description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    title: "Sameer Chauhan",
+    description: "I am a Full Stack Developer from India.",
+    url: "https://self.chauhan.org.in",
+    siteName: "self.chauhan.org.in",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://avatars.githubusercontent.com/u/48860013?v=4",
         width: 1920,
         height: 1080,
       },
@@ -38,7 +37,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Sameer Chauhan",
     card: "summary_large_image",
   },
   icons: {
